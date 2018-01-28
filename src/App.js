2819b0594f1a5a32637802634813e1f8baba575e
codeathon51 <codeathon51@wpsd.k12.pa.us>
2018-01-28 12:59:10 -0500

    contact

--- src/App.js
+++ src/App.js
@@ -2,9 +2,10 @@ import React, { Component } from 'react';
 //import logo from './logo.svg';
 import './App.css';
 import Home from "./Pages/Home";
-import Pollution from "./Pages/Pollution";
+//import Pollution from "./Pages/Pollution";
 import Profile from "./Pages/Profile";
 import Signup from "./Pages/Signup";
+import Contact from "./Pages/Contact";
 import NavBar from "./NavBar";
 
 import "./App.css";
@@ -12,7 +13,8 @@ import "./App.css";
 const pages = [
     { name: "Home", component: Home },
     { name: "Profile", component: Profile},
-    { name: "Signup", component: Signup }
+    { name: "Signup", component: Signup },
+    { name: "Contact Us", component: Contact}
 ];
 
 class App extends Component {
---
+++ src/Pages/Contact.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./Contact.css";
+export default () => (
+    <div>
+    <div class='background'>
+    <div class='container3'>
+      <div class='outer'>
+        <div class='outer-corner'></div><div class='outer-content'>Contact</div></div>
+      <div class='main'>
+        <div class='pd'><p class='st'>Email-Charlene Canning</p><a href='mailto:canning.charlene@williampennsd.net'>canning.charlene@williampennsd.net</a><p class='st'>Email-Ricquela McLeod</p><a href='mailto:avatarpinkies@gmail.com'>avatarpinkies@gmail.com</a><p class='st'>Email-Favour Olugbenga</p><a href='mailto:feevs1017@gmail.com'>feevs1017@gmail.com</a><p class='st'>Email-Martinique Shaw</p><a href='mailto:americanmoskau@gmail.com'>americanmoskau@gmail.com</a><p class='st'>Email-Joseph Tholley</p><a href='mailto:alphatholley20@gmail.com'>alphatholley20@gmail.com</a><p class='st'>Facebook</p><a href='https://www.facebook.com/middle.man.351756'>Favour Olugbenga Middle Man</a><p class='st'>Twitter</p><a href='https://twitter.com/middleman_the'>@Favour O-Middle Man</a></div>
+        <div class='map'></div>
+        <div class='img'></div>
+        <div class='main-outer'></div>
+      </div>
+    </div>
+    </div>
+    </div>
+ )
