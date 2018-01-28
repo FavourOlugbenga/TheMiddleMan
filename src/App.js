799c70a44bac70e0cc9258890cb5867313c1a3d3
codeathon51 <codeathon51@wpsd.k12.pa.us>
2018-01-27 19:40:33 -0500

    add navigation

--- src/App.js
+++ src/App.js
@@ -1,21 +1,46 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import Home from "./Pages/Home";
+import Pollution from "./Pages/Pollution";
+import Profile from "./Pages/Profile"
+import Signup from "./Pages/Signup"
+import NavBar from "./NavBar";
+
+import "./App.css";
+
+const pages = [
+    { name: "Home", component: Home },
+    { name: "Pollution", component: Pollution },
+    { name: "Profile", component: Profile },
+    { name: "Signup", component: Signup }
+];
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    constructor(props, context) {
+        super(props, context);
+
+        this.state = {
+            page: Home
+        };
+
+        this.navigate = this.navigate.bind(this);
+    }
+
+    navigate(page) {
+        this.setState({ page });
+    }
+
+    render() {
+        const Page = this.state.page;
+        return (
+            <div className="app">
+                <NavBar pages={pages} onChange={this.navigate} />
+                <Page />
+            </div>
+        );
+    }
 }
+
 
 export default App;
